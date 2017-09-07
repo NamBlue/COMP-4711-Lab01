c5c3e3622188f829424625e4b3e6e6d7f3c8b4d7
George Lee <namblue@me.com>
2017-09-07 16:22:45 -0700

    Completed all basic requirements

--- Student.php
+++ Student.php
@@ -7,12 +7,15 @@
  */
 
 /**
- * Description of Student
+ * Represents a student which holds information about itself
  *
  * @author namblue
  */
-class Student {
-    
+class Student 
+{
+    /**
+     * Initializes a new instance of a Student
+     */
     function __construct() 
     {
         $this->surname = '';
@@ -21,13 +24,48 @@ class Student {
         $this->grades = array();
     }
     
+    /**
+     * Adds a email address to a collection of emails for this Student
+     * @param type $which the index of where in the collection to add
+     * @param type $address the string representation of the email address to add
+     */
     function add_email($which,$address) 
     {
         $this->emails[$which] = $address;
     }
 
+    /**
+     * Adds a grade to the collection of grades for this Student
+     * @param type $grade the unsigned int representation of the grade to add
+     */
     function add_grade($grade) 
     {
         $this->grades[] = $grade;
+    }
+    
+    /**
+     * Gets the average for all grades for this Student
+     * @return type the average grades as a unsigned decimal
+     */
+    function average() 
+    {
+        $total = 0;
+        foreach ($this->grades as $value)
+            $total += $value;
+        return $total / count($this->grades);
+    }
+    
+    /**
+     * Gets the string representation of this Student
+     * @return type the Student as a string
+     */
+    function toString() 
+    {
+        $result = $this->first_name . ' ' . $this->surname;
+        $result .= ' ('.$this->average().")\n";
+        foreach($this->emails as $which=>$what)
+            $result .= $which . ': '. $what. "\n";
+        $result .= "\n";
+        return '<pre>'.$result.'</pre>';
     }
 }
--- index.php
+++ index.php
@@ -5,8 +5,47 @@
         <title></title>
     </head>
     <body>
-        <?php
-        echo 'Hello Out There!!!';
-        ?>
+<?php
+
+include('Student.php');
+
+$students = array();
+
+$first = new Student();
+$first->surname = "Doe";
+$first->first_name = "John";
+$first->add_email('home','john@doe.com');
+$first->add_email('work','jdoe@mcdonalds.com');
+$first->add_grade(65);
+$first->add_grade(75);
+$first->add_grade(55);
+$students['j123'] = $first;
+
+$second = new Student();
+$second->surname = "Einstein";
+$second->first_name = "Albert";
+$second->add_email('home','albert@braniacs.com');
+$second->add_email('work1','a_einstein@bcit.ca');
+$second->add_email('work2','albert@physics.mit.edu');
+$second->add_grade(95);
+$second->add_grade(80);
+$second->add_grade(50);
+$students['a456'] = $second;
+
+$third = new Student();
+$third->surname = "Lee";
+$third->first_name = "George";
+$third->add_email('work','jlee1147@my.bcit.ca');
+$third->add_grade(95);
+$third->add_grade(92);
+$third->add_grade(89);
+$students['a000'] = $third;
+
+ksort($students);
+
+foreach($students as $student)
+    echo $student->toString();
+
+?>
     </body>
 </html>
